fef94ceea50c65b0fe38671c8c70bee239b2498d
aashish366 <119336778+aashish366@users.noreply.github.com>
2024-07-30 08:20:40 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";  // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +   
+    3) //code readability should be high
+
+console.log("Aashish Gupta");
+
+let name="Aashish"
+let age=23
+let isLoggedIn=false
+let state; //It will be undefined because we did not assign the value in it.
+
+console.log(state);
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// symbol => unique
+
+// object
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
