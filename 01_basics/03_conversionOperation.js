32ba771c3e289392c789f5b64d9bb46b14bda49a
aashish366 <119336778+aashish366@users.noreply.github.com>
2024-07-31 09:40:03 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,68 @@
+let score="true";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber=Number(score);
+
+// console.log(valueInNumber);  //NaN(Not a Number)
+// console.log(typeof valueInNumber); //Number
+
+// "33"=> 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn ="Aashish";
+
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+//1 => ture; 0 => false
+//"" => false
+//"hitesh" => true 
+
+let someNumber=33
+
+let stringNumber = String(someNumber)
+
+// console.log(stringNumber);
+//console.log(typeof stringNumber)
+
+// *********************** Operations ***********************
+
+let value=3;
+let negValue=-value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="hello";
+let str2="Aashish"
+let str3=str1+str2;
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1=num2=num3=2+2;
+
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+let gameCounter=100;
+// gameCounter++;
+// console.log(gameCounter);
+
